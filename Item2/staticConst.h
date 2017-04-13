9c0264267d649997490ecefcf49999937c47103d
Udita Sharma <uditas15@gmail.com>
2017-04-13 15:57:44 +0530

    1.3

--- Item2/staticConst.h
+++ Item2/staticConst.h
@@ -7,7 +7,7 @@ class A
     public:
         A():i(0)
         {
-            cout<<"Hello"<<mem1;
+            cout<<"Hello"<<mem1;//mem1 is static const, its scope is till program execution
         }
     private:
         static const double mem1;
