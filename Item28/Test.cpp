797f5ebcd6db11a216d221b2a7b3c5e50fa079cd
Udita Vinod Sharma <usharma@manh.com>
2017-06-17 17:48:54 +0530

    1.2

--- Item28/Test.cpp
+++ Item28/Test.cpp
@@ -26,12 +26,38 @@ struct RecData
 {
 	Point ulhc;//upper left hand corner
 	Point lrhc;//lower right hand corner
+	
+public:
+	RecData(Point u, Point l) :ulhc(u), lrhc(l)
+	{}
 };
 
 class Rectangle
 {
 public:
-	Point &
+	Rectangle(Point p1, Point p2)
+	{
+	}
+	Point &upperLeft() const
+	{
+		return pData->ulhc;
+	}
+
+	Point &lowerRight() const
+	{
+		return pData->lrhc;
+	}
 private:
+	RecData* pData;
 
 };
+
+int main()
+{
+	Point cord1(0, 0);
+	Point cord2(100, 100);
+	const Rectangle rec(cord1, cord2);
+	rec.upperLeft().setX(50);//Even though rec is const we can still modify it
+	
+	return 0;
+}
